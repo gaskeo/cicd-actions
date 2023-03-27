91b5f04b12db87f9a5e07ce13823533a31a3e2d8
gaskeo <zeytunyan2004@mail.ru>
2023-03-27 15:09:30 +0500

    feat(handlers): new scheme for /expression handler

--- handlers/expression_handler.py
+++ handlers/expression_handler.py
@@ -1,13 +1,16 @@
 from typing import Literal, Type
 
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, status
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 
 
-class Expression(BaseModel):
-    number1: float
-    number2: float
-    operation: Literal["+", "-", "*", "/"]
+class DetailExceptionResponse(BaseModel):
+    detail: str
+
+
+class ExpressionResponse(BaseModel):
+    answer: float
 
 
 class ExpressionSolver:
@@ -40,19 +43,21 @@ class DivisionSolver(ExpressionSolver):
 
 
 def create_expression_handler(app: FastAPI):
-    @app.get("/expression")
-    def expression_handler(expression: Expression):
+    @app.get("/expression", response_model=ExpressionResponse, responses={
+        status.HTTP_451_UNAVAILABLE_FOR_LEGAL_REASONS: {"model": DetailExceptionResponse}
+    })
+    def expression_handler(number1: float, number2: float, operation: Literal["+", "-", "*", "/"]):
         expression_solver: Type[ExpressionSolver] = ...
-        if expression.operation == "+":
+        if operation == "+":
             expression_solver = SumSolver
-        if expression.operation == "-":
+        if operation == "-":
             expression_solver = DifferenceSolver
-        if expression.operation == "*":
+        if operation == "*":
             expression_solver = MultiplicationSolver
-        if expression.operation == "/":
+        if operation == "/":
             expression_solver = DivisionSolver
         try:
-            return expression_solver(expression.number1, expression.number2).solve()
+            return {"answer": expression_solver(number1, number2).solve()}
         except ZeroDivisionError:
-            raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Division by zero")
-
+            return JSONResponse(status_code=status.HTTP_451_UNAVAILABLE_FOR_LEGAL_REASONS,
+                                content={"detail": "Division by zero"})
